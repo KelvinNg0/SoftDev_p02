77a6f09e7f1afe4203d3e011a89996642c069964
Kevin Li <kevinli4321@gmail.com>
2020-01-07 17:49:33 -0500

    add_perks backbone is done, now a matter of coming up with perks

--- utl/db_creation.py
+++ utl/db_creation.py
@@ -1,3 +1,5 @@
+#Run this file to generate an empty database file. Do not run this in any other case.
+
 import sqlite3
 from datetime import datetime #for timestamps
 
@@ -34,7 +36,6 @@ def init_db():
         )
         """
     )
-    add_perks()
 
     #Create achievements table (empty)
     c.execute(
@@ -52,16 +53,18 @@ def init_db():
 
 
 def add_perks():
+    db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+    c = db.cursor()
+
     #In order: ID, name of perk, description of perk, cost of perk
-    c.execute(
-        """
-        INSERT INTO perks VALUES (?, ?, ?, ?),
-          (0,
-          "Autoclicker 1",
-          "Generates clicks while idle!",
-          50
-          )
-        """
-    )
+    c.execute("INSERT INTO perks VALUES (?, ?, ?, ?)", (0, "Autoclicker 1", "Generates clicks while idle!", 20))
+    c.execute("INSERT INTO perks VALUES (?, ?, ?, ?)", (1, "Autoclicker 2", "Generates clicks while idle!", 100))
+    c.execute("INSERT INTO perks VALUES (?, ?, ?, ?)", (2, "Autoclicker 3", "Generates clicks while idle!", 250))
+    c.execute("INSERT INTO perks VALUES (?, ?, ?, ?)", (3, "Autoclicker 4", "Generates clicks while idle!", 500))
+    c.execute("INSERT INTO perks VALUES (?, ?, ?, ?)", (4, "Autoclicker 5", "Generates clicks while idle!", 1000))
+
+    db.commit() #save changes
+    db.close()
 
 init_db();
+add_perks();
