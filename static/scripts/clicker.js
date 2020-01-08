e86d18930a49c407a4c87d43d2128fd5a4b708db
jjshawshaw <jshaw00@stuy.edu>
2020-01-08 00:08:05 -0500

    better clicc

--- static/scripts/clicker.js
+++ static/scripts/clicker.js
@@ -6,9 +6,9 @@ var scrollover = function(e){
   var rect = e.target.getBoundingClientRect();
   if (Math.sqrt(((rect["x"] + rect["width"] / 2) - e.clientX)**2 + ((rect["y"] + rect["width"] / 2) - e.clientY)**2) <= (rect["width"] / 2)){
     e.target.style.cursor = "pointer";
-    e.target.style.height= '99.75%';
-    e.target.style.width= '99.75%';
-    document.getElementById("filler").style.height= '29.75%';
+    e.target.style.height= '99.95%';
+    e.target.style.width= '99.95%';
+    document.getElementById("filler").style.height= '29.8%';
   } else{
     e.target.style.height= '98%';
     e.target.style.width= '98%';
@@ -21,15 +21,13 @@ var cookie_click = function(e){
   console.log("click!");
   var rect = e.target.getBoundingClientRect();
   if (Math.sqrt(((rect["x"] + rect["width"] / 2) - e.clientX)**2 + ((rect["y"] + rect["width"] / 2) - e.clientY)**2) <= (rect["width"] / 2)){
-    e.target.style.height= '98%';
-    e.target.style.width= '98%';
-    e.target.style.cursor = "auto";
-    document.getElementById("filler").style.height= '30%';
+    e.target.style.height= '96%';
+    e.target.style.width= '96%';
+    document.getElementById("filler").style.height= '30.25%';
     setTimeout(() => {
-      e.target.style.cursor = "pointer";
-      e.target.style.height= '99.75%';
-      e.target.style.width= '99.75%';
-      document.getElementById("filler").style.height= '29.75%';
+      e.target.style.height= '99.95%';
+      e.target.style.width= '99.95%';
+      document.getElementById("filler").style.height= '29.8%';
     }, 150);
   };
 };
