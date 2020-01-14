479b727212fe7379146ae08e112e32193490f420
Kevin Li <kevinli4321@gmail.com>
2020-01-14 18:27:03 -0500

    Make the sign up button on the front page look less awkwardly placed

--- static/scripts/trial.js
+++ static/scripts/trial.js
@@ -1,25 +1,25 @@
 var cookie_img = document.getElementById("cookie-img");
-cookies_clicked = 0;
 var timer = document.getElementById("timer");
 var trial_type = document.getElementById("trialtype");
 
-var scrollover = function(e){
+cookies_clicked = 0;
+
+var scrollover = function(e) {
   var rect = e.target.getBoundingClientRect();
   if (Math.sqrt(((rect["x"] + rect["width"] / 2) - e.clientX)**2 + ((rect["y"] + rect["width"] / 2) - e.clientY)**2) <= (rect["width"] / 2)){
     e.target.style.cursor = "pointer";
     e.target.style.height= '99.95%';
     e.target.style.width= '99.95%';
     document.getElementById("filler").style.height= '29.8%';
-  } else{
+  } else {
     e.target.style.height= '98%';
     e.target.style.width= '98%';
     e.target.style.cursor = "auto";
     document.getElementById("filler").style.height= '30%';
-  };
+  }
 };
 
-var cookie_click = function(e){
-  console.log("click!");
+var cookie_click = function(e) {
   var rect = e.target.getBoundingClientRect();
   if (Math.sqrt(((rect["x"] + rect["width"] / 2) - e.clientX)**2 + ((rect["y"] + rect["width"] / 2) - e.clientY)**2) <= (rect["width"] / 2)){
     e.target.style.height= '96%';
@@ -39,25 +39,21 @@ var pass_trial_data = function() {
   $.ajax({
     url: "/recordtrial",
     data: {clicks: cookies_clicked, trial_type: trial_type.options[trial_type.selectedIndex].value},
-    success: function (data) {
-      console.log("Data received.");
-    }
   });
 }
 
-var start_timer = function() {
-  var trial_type = document.getElementById("trialtype");
+var start_trial = function() {
   var seconds = trial_type.options[trial_type.selectedIndex].value;
   trial_type.disabled = true;
-
-  cookie_img.addEventListener('click', cookie_click);
   document.getElementById("starttimer").disabled = true;
+  cookie_img.addEventListener('click', cookie_click);
+
   var timeInterval = setInterval(function() {
     if (seconds == 1) {
       console.log("Trial complete!");
       timer.innerHTML = 0;
       clearInterval(timeInterval);
-      setTimeout(function() { alert("Trial complete! This run will be recorded."); }, 250);
+      setTimeout(function() { alert("Trial complete! This run will be recorded."); }, 250); //otherwise the timer doesn't set to 0 until the alert is dismissed
       cookie_img.removeEventListener('click', cookie_click);
       pass_trial_data();
 
@@ -71,9 +67,6 @@ var start_timer = function() {
   }, 1000);
 }
 
-//only allow this when the timer has been started
-//cookie_img.addEventListener('click', cookie_click);
+document.getElementById("starttimer").addEventListener('click', start_timer);
 cookie_img.addEventListener('mousemove', scrollover);
 cookie_img.addEventListener('mouseout', scrollover);
-
-document.getElementById("starttimer").addEventListener('click', start_timer);
